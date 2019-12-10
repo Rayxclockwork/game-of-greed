e58d692afa571c2d89140442a4229889505c36f5
RAVEN DELANEY <ravend17@gmail.com>
2019-12-09 17:40:35 -0800

    greeting and prompt work

--- game_of_greed.py
+++ game_of_greed.py
@@ -1,1 +1,17 @@
+import random
+
+class Game:
+
+
+    def play():
+        print('Welcome to Game of Greed')
+        answer = input('Wanna play? ')
+
+        if answer == 'y':
+            print('Great! Check back tomorrow. :D')
+        else:
+            print('OK. Maybe another time.')
+    play()
+
+    def calculate_score():
 
