fdbdf2fb627044b5cc554c89b4fa433b797bb7a1
RAVEN DELANEY <ravend17@gmail.com>
2019-12-10 20:38:33 -0800

    most code works, but doesn't add correctly, 8 tests passing

--- test_game_of_greed.py
+++ test_game_of_greed.py
@@ -1,4 +1,4 @@
-import random
+# import random
 import pytest
 from game_of_greed import Game
 
@@ -8,78 +8,122 @@ def test_game_instance():
     assert game
 
 
-def test_greeting():
-    prints = ["Welcome to Game of Greed"]
-    prompts = ['Wanna play? ']
-    responses = []
+# def test_greeting():
+#     prints = ["Welcome to Game of Greed"]
+#     prompts = ['Wanna play? ']
+#     responses = ['y']
 
-    def testing_print(message):
-        if len(prints):
-            assert message == prints.pop(0)
+#     def testing_print(*args):
+#         if len(prints):
+#             current_print = prints.pop(0)
+#             assert args[0] == current_print
 
-    def testing_input(prompt):
-        if len(prompts):
-            assert prompt == prompt.pop(0)
+#     def testing_input(*args):
+#         if len(prompts):
+#             current_prompt = prompts.pop(0)
+#             assert args[0] == current_prompt
 
-    game = Game(testing_print, testing_input)
-    game.game()
+#         if len(responses):
+#             current_response = responses.pop(0)
+#             return current_response
+
+#     game = Game(testing_print, testing_input)
+#     game.play()
+
+
+def test_calculate_score(game, dice, expected):
+    actual = game.calculate_score(dice)
+    assert actual == expected
+
+
+def test_calc_score_simple():
+    g = Game()
+    actual = g.calculate_score((1, 2))
+    expected = 100
+    assert expected == actual
+
+
+def test_zilch(game):
+    expected = 0
+    actual = game.calculate_score((2, 3, 4, 6, 2, 3))
+    assert actual == expected
+
+
+def test_ones(game):
+    expected = 4000
+    actual = game.calculate_score((1, 1, 1, 1, 1, 1))
+    assert actual == expected
+
+
+def test_twos(game):
+    expected = 400
+    actual = game.calculate_score((4, 2, 2, 6, 2, 2))
+    assert actual == expected
+
+
+def test_threes(game):
+    expected = 300
+    actual = game.calculate_score((6, 3, 2, 3, 3, 4))
+    assert actual == expected
+
+
+def test_fours(game):
+    expected = 1600
+    actual = game.calculate_score((4, 4, 4, 4, 4, 4))
+    assert actual == expected
+
+
+def test_fives(game):
+    expected = 1000
+    actual = game.calculate_score((5, 6, 4, 5, 5, 5))
+    assert actual == expected
+
+
+def test_sixes(game):
+    expected = 600
+    actual = game.calculate_score((2, 6, 3, 6, 6, 4))
+    assert actual == expected
+
+
+def test_straight(game):
+    expected = 1500
+    actual = game.calculate_score((6, 3, 4, 2, 1, 5))
+    assert expected == actual
+
+
+def test_mcflurry(game):
+    expected = 2000
+    actual = game.calculate_score((5, 1, 5, 5, 6, 5))
+    assert expected == actual
+
+
+def test_three_pairs(game):
+    expected = 1500
+    actual = game.calculate_score((5, 4, 1, 5, 1, 4))
+    assert expected == actual
 
 
 # @pytest.mark.skip('pending')
-# def test_zilch():
-#     # # non scoring roll should return 0
+# def test_two_trios(game):
+#     expected =
+#     actual = game.calculate_score(())
+#     assert actual == expected
 
 
 # @pytest.mark.skip('pending')
-# def test_ones():
-#     # # rolls with various number of 1s should return correct score
+# def test_leftover_ones(game):
+#     expected =
+#     actual = game.calculate_score(())
+#     assert actual == expected
 
 
 # @pytest.mark.skip('pending')
-# def test_twos():
-#     # # rolls with various number of 2s should return correct score
+# def test_leftover_fives(game):
+#     expected =
+#     actual = game.calculate_score(())
+#     assert actual == expected
 
 
-# @pytest.mark.skip('pending')
-# def test_threes():
-#     # # rolls with various number of 3s should return correct score
-
-
-# @pytest.mark.skip('pending')
-# def test_fours():
-#     # # rolls with various number of 4s should return correct score
-
-
-# @pytest.mark.skip('pending')
-# def test_fives():
-#     # # rolls with various number of 5s should return correct score
-
-
-# @pytest.mark.skip('pending')
-# def test_sixes():
-#     # # rolls with various number of 6s should return correct score
-
-
-# @pytest.mark.skip('pending')
-# def test_straight():
-#     # # 1,2,3,4,5,6 should return correct score
-
-
-# @pytest.mark.skip('pending')
-# def test_three_pairs():
-#     # # 3 pairs should return correct score
-
-
-# @pytest.mark.skip('pending')
-# def test_two_trios():
-#     # # 2 sets of 3 should return correct score
-
-
-# @pytest.mark.skip('pending')
-# def test_leftover_ones():
-#     # # 1s not used in set of 3 (or greater) should return correct score
-
-
-# @pytest.mark.skip('pending')
-# def test_leftover_fives():
-#     # # 5s not used in set of 3 (or greater) should return correct score
+@pytest.fixture()
+def game():
+    return Game()
