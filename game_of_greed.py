47cae631b6c752ee6efde66f6891ab65903522ab
RAVEN DELANEY <ravend17@gmail.com>
2019-12-11 20:13:35 -0800

    some code for turns and added class demo tests

--- game_of_greed.py
+++ game_of_greed.py
@@ -1,14 +1,13 @@
 import random
 import collections
 
-
 class Game:
     def __init__(self, print_func=print, input_func=input):
         """initiates new instance of the game"""
         self._print = print_func
         self._input = input_func
 
-    def play(self):
+    def begin(self):
         """begins the game by welcoming user, prompting them to begin"""
         print('Welcome to Game of Greed')
         answer = input('Wanna play? ')
@@ -18,7 +17,8 @@ class Game:
         else:
             print('OK. Maybe another time.')
 
-    def _do_roll(self, num_dice):
+    def dice_roll(self, num_dice):
+        num_dice = 6
         return tuple(random.randint(1, 6) for i in range(num_dice))
 
     def calculate_score(self, dice):
@@ -30,26 +30,19 @@ class Game:
         """Grand Mcflurry"""
         if count[5] == 4 and count[1] == 1:
             score += 2000
-            return score
+            return 2000
 
         """Straights"""
         if 1 in count and 2 in count and 3 in count and 4 in count and 5 in count and 6 in count:
             score += 1500
-            return score
+            return 1500
 
         """Pairs"""
         for i in count:
             if count[i] == 2:
                 pairs += 1
                 if pairs == 3:
-                    score = 0
-                    score += 1500
-                    return score
-
-            """Three of a kind for anything other than ones"""
-            if count[i] == 3 and i != 1:
-                score += (i * 100)
-                continue
+                    return 1500
 
             """Less than 3 fives"""
             if count[i] < 3 and i == 5:
@@ -60,10 +53,36 @@ class Game:
             if count[i] < 3 and i == 1:
                 score += (count[i] * 100)
 
-            return score
+            """handles 3 or more of anything other than 1s"""
+            if count[i] >= 3 and i != 1:
+                score += ((count[i] - 2)*(i * 100))
 
+            """handles 3 or more 1s"""
+            if count[i] >= 3 and i == 1:
+                score += ((count[i] -2)*(i * 1000))
+
+        return score
+
+    def each_turn(self):
+        """handles each turn"""
+        score = 0
+        while score == 0:
+            dice_roll()
+            calculate_score()
+            if score == 0:
+                break
+            else:
+                print('You got ' + score + ' points')
+                print('Would you like to bank points or roll again')
+                if input('bank'):
+                    dice_roll()
+                else:
+                    return score
+                break
+        #roll dice, calc score, prompt for save dice or bank points
+        # if zilch, new turn
 
 if __name__ == "__main__":
     game = Game()
 
-    game.play()
+    game.begin()
